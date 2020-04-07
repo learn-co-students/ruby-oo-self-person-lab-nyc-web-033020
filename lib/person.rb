032350a8d394617ce9280a26035c0c244e924530
github username <github email address>
2020-04-06 22:40:51 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,11 @@
-# your code goes here
+class person
+
+  attr_reader :name
+  attr_accessor :bank_account
+
+  def initialize(name, bank_account= 25)
+    @name = name
+    @bank_account = bank_account
+  end
+
+end
