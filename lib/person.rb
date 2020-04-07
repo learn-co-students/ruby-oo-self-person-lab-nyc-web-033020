346c6f38cd6a7db6e7c18dbe6750bb915b871c16
github username <github email address>
2020-04-06 23:12:22 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,77 @@
-class person
+class Person
 
   attr_reader :name
-  attr_accessor :bank_account
+  attr_accessor :bank_account, :happiness, :hygiene
 
-  def initialize(name, bank_account= 25)
+  def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
     @name = name
     @bank_account = bank_account
+    @happiness = happiness
+    @hygiene = hygiene
+  end
+
+  def happiness=(num)
+    if num > 10
+      @happiness = 10
+    elsif num < 0
+      @happiness = 0
+    else
+      @happiness = num
+    end
+  end
+
+  def hygiene=(num)
+    if num > 10
+      @hygiene = 10
+    elsif num < 0
+      @hygiene = 0
+    else
+      @hygiene = num
+    end
+  end
+
+  def happy?
+    @happiness > 7
+  end
+
+  def clean?
+    @hygiene > 7
+  end
+
+  def get_paid(salary)
+    @bank_account += salary
+    p "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    p "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    p "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    p "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(person, topic)
+    if topic == "politics"
+      self.happiness -= 2
+      person.happiness -= 2
+      p "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+      self.happiness += 1
+      person.happiness += 1
+      p "blah blah sun blah rain"
+    else
+      p "blah blah blah blah blah"
+    end
   end
 
 end
